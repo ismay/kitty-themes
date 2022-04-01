8a99439bc59ddf3e182cf2bdfbc1fbfb5b28953c
ismay <7355199+ismay@users.noreply.github.com>
2022-04-01 18:41:21 +0200

    Ensure gomplate is installed

--- build.fish
+++ build.fish
@@ -1,6 +1,11 @@
 #!/usr/bin/env fish
 
 for data_source in (status dirname)/data/*.yaml
+  if ! type -q gomplate
+    echo "This script needs gomplate to be installed"
+    return
+  end
+
   gomplate \
     -d theme="$data_source" \
     -f ./templates/kitty.tmpl \
